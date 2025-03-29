8131632c1328bb550a7e93c97d6f1377fe3f5d36
Kushaagra <mehtakushaagra@gmail.com>
2025-03-29 12:03:23 +0530

    transform complete

--- journal.py
+++ journal.py
@@ -3,6 +3,13 @@ import whisper
 import tempfile
 model = whisper.load_model("base")
 import os
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+load_dotenv()
+from prompts import JOURNAL_PROMPT
+API = os.environ['GROQ_API']
+llm = ChatGroq(model="gemma2-9b-it", api_key=API)
+
 def transcribe_audio(audio_file):
 
     current_directory = os.path.dirname(os.path.abspath(__file__))
@@ -36,7 +43,19 @@ if upload:
         
         if st.button("Summarise"):
             with st.spinner("Summarising the Audio..."):
-                transcription = transcribe_audio(audio_file)
+                transcription = """ Woke up late today, way past my alarm. Felt groggy, probably should sleep earlier. Checked my phone first thing—bad habit, I know. Emails, messages, some news, nothing exciting. Had a quick breakfast, just coffee and a slice of bread, then straight to work.
+
+Spent way too long on a stupid bug in my AI model. Thought it was a major issue, turns out I just missed a small config change. Annoying but at least it’s fixed. Had a meeting after that, mostly about optimizing data pipelines. Some parts were interesting, but honestly, I was zoning out halfway through. Too much theory, not enough action.
+
+Lunch was whatever, just a sandwich. I should start cooking real meals. Afternoon was a blur, some coding, some scrolling, not as productive as I wanted. Went for a run in the evening, which helped clear my head. Caught up with a few friends later, random conversations about tech, life, and stupid things from college.
+
+Now just lying in bed, feeling kind of exhausted but also like I didn’t do enough. Some progress, but nothing major. Maybe tomorrow will be better."""
+                # transcription = transcribe_audio(audio_file)
                 st.text_area("Transcription", value=transcription, height=300)
+                st.write("done")
+                pro = JOURNAL_PROMPT + transcription
+                res = llm.invoke(pro)
+                st.write(pro)
+                st.write(res)
                 
 
--- prompts.py
+++ prompts.py
@@ -3,13 +3,9 @@ JOURNAL_PROMPT = """
     Your goal is to prepare a journal with the information of the events which happened in the given text.
     You have to preserve all the important events which happened and whatever important information is present in the given text.
     
+    Do not write in an informal manner, rephrase it in short and write a crisp and short summary with pointwise events
     Note: transform this text into a single day's journal and write them like a person would write in their diary, but in the form of sequential events
-
-    ------------
-    {text}
-    ------------
-
     
+    The given text: 
 
-    The Journal timeline is
 """
